474d5d119e9e1974c661739b074061e1ca0a266b
Alejandra C. Araya Díaz <Arayadiaz.ac@gmail.com>
2023-08-31 21:56:54 -0300

    update

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
 /*------------------------------------metodos---------------------------------------------------- 
-
-
+Document.getElementsByClassName(String className)
+Document.getElementsByTagName(String tagName)
+Document.getElementById(String id)
+Document.querySelector(String selector)
+Document.querySelectorAll(String selector)
+Document.createDocumentFragment()
+Document.createElement(String name)
+#getElementById
 -----------------------------------------------------------------------------------------------*/
+// document.addEventListener("DOMContentLoaded"){
+
+//     console.log(document.getElementById("tituloWeb"));
+//     console.log(document.getElementById("tituloWeb").textContent);
+
+// };//permite carga el navegador html y arbol dom esta construido.perp el codigo debe estar dentro
+
+console.log(document.getElementById("tituloWeb"));
+console.log(document.getElementById("tituloWeb").textContent);
+console.log(document.querySelector( " .text-primary "));//selecciona el primer elemento
+console.log(document.querySelectorAll(".text-danger"));
+
+//todos estos elementos seleccionados generalmente se guardan:
+
+const h1 = document.querySelector("h1.tagName");
+console.log(h1);
+//className es una propiedad que muestra el nombre de la clase que lleva el elemento
